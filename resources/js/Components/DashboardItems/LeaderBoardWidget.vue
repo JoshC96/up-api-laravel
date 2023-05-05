97c824a669b2081f9c017783320c0583e854d78e
Josh Campbell <josh.campbell4000@gmail.com>
2023-05-05 23:25:29 +0930

    add title and fix type error on leaderboard widget

--- resources/js/Components/DashboardItems/LeaderBoardWidget.vue
+++ resources/js/Components/DashboardItems/LeaderBoardWidget.vue
@@ -1,11 +1,12 @@
 <template>
     <div class="w-full px-6 sm:w-1/2 xl:w-1/3 mb-5">
+        <h3 class="h3 text-2xl mb-5">{{ title }}</h3>
         <div v-for="(item, index) in items" :key="index"
             class="flex items-center px-5 py-6 bg-white shadow-sm">
             <div class="p-3 bg-indigo-600 bg-opacity-75 rounded-full text-white min-w-[50px] text-center"> {{ item.transaction_count }} </div>
             <div class="mx-5">
-                <h4 class="text-2xl font-semibold text-gray-700">{{ formatCurrency(item.total_amount) }}</h4>
-                <div class="text-gray-500 capitalize">{{ index.replace(/-/g, ' ') }}</div>
+                <h4 class="text-2x1 font-semibold text-gray-700">{{ formatCurrency(item.total_amount) }}</h4>
+                <div class="text-gray-500 capitalize">{{ index.toString().replace(/-/g, ' ') }}</div>
             </div>
         </div>
     </div>
@@ -20,6 +21,7 @@ interface Item {
 }
 
 defineProps({
+    title: String,
     items: Array<Item>,
 })
 
